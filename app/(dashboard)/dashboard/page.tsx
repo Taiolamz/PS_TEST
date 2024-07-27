164cb75be30da3c62c77f1a0ca19ff178b9f9b42
Emydev-op <alphaoseghe@gmail.com>
2024-07-27 23:05:24 +0100

    complete sidebar collaspe

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { DashboardLayout } from './_components/DashboardLayout';
+import Link from 'next/link';
+import Routes from '@/app/_Routes/routes';
 
 const Dashboard = () => {
     return (
-        <DashboardLayout>
-          <p className=''>Dashboard</p>  
-        </DashboardLayout>
+      <DashboardLayout>
+        <Link
+          href={Routes.ChecklistRoute.ChecklistOverview()}
+          className=" text-primary font-semibold text-sm"
+        >
+          <p className="p-4 font-semibold underline bg-[#FFFCC2]">Setup Checklist...</p>
+        </Link>
+      </DashboardLayout>
     );
 }
 
