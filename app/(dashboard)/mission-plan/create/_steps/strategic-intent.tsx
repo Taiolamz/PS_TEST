cac392bf9b6af6615c120cc539451e3127369d5e
Odizee <odizeeplatform@gmail.com>
2024-08-02 22:41:18 +0100

    added navigation to next step

--- app/(dashboard)/mission-plan/create/_steps/strategic-intent.tsx
+++ app/(dashboard)/mission-plan/create/_steps/strategic-intent.tsx
@@ -11,6 +11,8 @@ import { BsFillInfoCircleFill } from "react-icons/bs";
 import { v4 as uuidv4 } from "uuid";
 import * as Yup from "yup";
 import { cn } from "@/lib/utils";
+import { useAddStrategicIntentMutation } from "@/redux/services/mission-plan/missionPlanApi";
+import { usePathname, useRouter } from "next/navigation";
 
 const validationSchema = Yup.object().shape({
   // mission_plan_id: Yup.string().required("Mission Plan ID is required"),
@@ -20,7 +22,7 @@ const validationSchema = Yup.object().shape({
       intent: Yup.string().required("Intent is required"),
       behaviours: Yup.array().of(
         Yup.object().shape({
-          id: Yup.string().required(),
+          id: Yup.string(),
           value: Yup.string().required("Behaviour is required"),
         })
       ),
@@ -29,42 +31,43 @@ const validationSchema = Yup.object().shape({
 });
 
 const StrategicIntent = () => {
-  //   const [
-  //     createMissionPlanStrategicIntent,
-  //     { isLoading: isLoadingStrategicIntent },
-  //   ] = useCreateMissionPlanStrategicIntentMutation();
+  const location = usePathname();
+  const router = useRouter();
 
-  //   const handleSaveStrategicIntent = async () => {
-  //     const { intents, mission_plan_id, strategic_intent_id } = formik.values;
+  const [addStrategicIntent, { isLoading: isLoadingStrategicIntent }] =
+    useAddStrategicIntentMutation();
 
-  //     const transformedIntents = {
-  //       intents: intents.map(
-  //         (intent: { intent: any; behaviours: { value: any }[] }) => ({
-  //           intent: intent.intent,
-  //           behaviours: intent.behaviours.map(
-  //             (behaviour: { value: any }) => behaviour.value
-  //           ),
-  //         })
-  //       ),
-  //       mission_plan_id,
-  //       strategic_intent_id,
-  //     };
+  const handleSaveStrategicIntent = async () => {
+    const { intents, mission_plan_id, strategic_intent_id } = formik.values;
 
-  //     await createMissionPlanStrategicIntent(transformedIntents);
-  //   };
+    const transformedIntents = {
+      intents: intents.map(
+        (intent: { intent: any; behaviours: { value: any }[] }) => ({
+          intent: intent.intent,
+          behaviours: intent.behaviours.map(
+            (behaviour: { value: any }) => behaviour.value
+          ),
+        })
+      ),
+      mission_plan_id,
+      strategic_intent_id,
+    };
+
+    await addStrategicIntent(transformedIntents);
+  };
 
   const handleChange = (
     value: string,
-    intentIndex: number,
-    behaviourIndex?: number
+    index: number,
+    behaviourIndex?: number | undefined
   ) => {
-    if (typeof behaviourIndex === "number") {
+    if (typeof behaviourIndex === "undefined") {
+      formik.setFieldValue(`intents.${index}.intent`, value);
+    } else {
       formik.setFieldValue(
-        `intents.${intentIndex}.behaviours.${behaviourIndex}.value`,
+        `intents.${index}.behaviours.${behaviourIndex}.value`,
         value
       );
-    } else {
-      formik.setFieldValue(`intents.${intentIndex}.intent`, value);
     }
   };
 
@@ -87,7 +90,7 @@ const StrategicIntent = () => {
 
   return (
     <div>
-      <div className="flex items-center gap-x-2 mb-5 px-1">
+      <div className="flex items-center gap-x-2 mb-8">
         <h1 className="text-[#3E4345]">Strategic Intent</h1>
         <span>
           <BsFillInfoCircleFill color="#84919A" />
@@ -101,21 +104,20 @@ const StrategicIntent = () => {
                 {formik.values.intents?.length > 0 &&
                   formik.values.intents.map((intent: any, index: number) => (
                     <div
-                      key={index}
+                      key={intent.id}
                       className="grid gap-y-5 items-center space-x-2 w-full mb-5 relative"
                     >
-                      <div>
+                      <div className="!ml-0">
                         <Textarea
                           rows={4}
-                          id="vision"
-                          label="Strategic Intent 1"
+                          id={`intent-${intent.id}`}
+                          label={`Strategic Intent ${index + 1}`}
                           labelClass="text-[#6E7C87] text-[13px] mb-[6px]"
                           name={`intents.${index}.intent`}
-                          // onChange={(e) => handleChange(e.target.value, index)}
                           error={errorIntents?.intent}
                           onChange={(e) => handleChange(e.target.value, index)}
-                          className="border p-2 w-full bg-[#F6F8F9]"
-                          value={formik.values.intents.intent}
+                          className="border p-2 md:min-w-[27rem] lg:min-w-[37rem] bg-[#F6F8F9]"
+                          value={formik.values.intents[index].intent}
                         />
                         <ErrorMessage
                           name={`intents.${index}.intent`}
@@ -227,13 +229,13 @@ const StrategicIntent = () => {
               type="submit"
               //   disabled={isLoadingStrategicIntent}
               //   loading={isLoadingStrategicIntent}
+              onClick={() => router.push(`${location}?ui=specified-intent`)}
               loadingText="Save & Continue"
               className={cn(
                 "w-full",
-                // !formik.isValid || isLoadingStrategicIntent
-                //   ? "opacity-50 cursor-not-allowed w-max"
-                //   :
-                "cursor-pointer text-white py-5 px-2 rounded-sm bg-primary border border-primary w-max"
+                !formik.isValid || isLoadingStrategicIntent
+                  ? "opacity-50 cursor-not-allowed w-max"
+                  : "cursor-pointer text-white py-5 px-2 rounded-sm bg-primary border border-primary w-max"
               )}
               // className={`text-white py-5 px-2 rounded-sm bg-primary border border-primary min-w-28`}
             >
