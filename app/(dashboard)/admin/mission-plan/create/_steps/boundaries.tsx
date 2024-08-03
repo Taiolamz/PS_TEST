a4b6f8bdc0e963a9778b449a0f1ede914f41afd6
TheOnis <onisamuelmichael@gmail.com>
2024-08-03 15:59:23 +0100

    Feat: conversion for MD mission plan detailed view Moved

--- app/(dashboard)/admin/mission-plan/create/_steps/boundaries.tsx
+++ app/(dashboard)/admin/mission-plan/create/_steps/boundaries.tsx
@@ -1,10 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import MissionDetailPreview from "./preview";
 
 const Boundaries = () => {
-    return (
+  const router = useRouter();
+  const location = usePathname();
+  const step = useSearchParams().get("step");
+  return (
+    <>
+      {step !== "preview" && (
         <div>
-            <h1>Freedom & Constraint</h1>
+          <h1>Freedom & Constraint</h1>
+          <Button
+            className="border"
+            type="button"
+            onClick={() =>
+              router.push(`${location}?ui=boundaries&step=preview`)
+            }
+          >
+            Save & Continue
+          </Button>
         </div>
-    );
-}
+      )}
+      {step === "preview" && <MissionDetailPreview />}
+    </>
+  );
+};
 
 export default Boundaries;
--- components/dashboard/missionplan/table/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MeasureOfSuccessTable } from "./measureOfSuccessTable";
