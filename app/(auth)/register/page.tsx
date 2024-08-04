a1c553ae4ebaece9b2f4087fa5340613405a8132
drew-chidi <chidi.andrew@gmail.com>
2024-08-04 18:35:51 +0100

    signup flow cleanup

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,28 +1,28 @@
-"use client";
-import ConfirmationModal from "@/components/atoms/modals/confirm";
-import { InputOTPGenerator } from "@/components/otp-generator";
-import { Button } from "@/components/ui/button";
-import routesPath from "@/utils/routes";
-import { RegistrationSchema } from "@/utils/schema";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
-import { useFormik } from "formik";
-import Link from "next/link";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import EmployeeInformation from "./_steps/employee-information";
-import OrganizationInformation from "./_steps/organization-information";
+'use client';
+import ConfirmationModal from '@/components/atoms/modals/confirm';
+import { InputOTPGenerator } from '@/components/otp-generator';
+import { Button } from '@/components/ui/button';
+import routesPath from '@/utils/routes';
+import { RegistrationSchema } from '@/utils/schema';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
+import { useFormik } from 'formik';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import EmployeeInformation from './_steps/employee-information';
+import OrganizationInformation from './_steps/organization-information';
 import {
   useAdminResendOTPMutation,
   useAdminVerifyOTPMutation,
   useRegisterMutation,
-} from "@/redux/services/auth/authApi";
-import Cookies from "js-cookie";
-import { toast } from "sonner";
-import { useEffect, useState } from "react";
-import { useAppDispatch } from "@/redux/store";
-import { resetAuth } from "@/redux/features/auth/authSlice";
-import useTimeout from "@/utils/hooks/useTimeout";
-import { timeToMinuteSecond } from "@/utils/helpers";
-import LoadingModal from "@/components/atoms/modals/loading";
+} from '@/redux/services/auth/authApi';
+import Cookies from 'js-cookie';
+import { toast } from 'sonner';
+import { useEffect, useState } from 'react';
+import { useAppDispatch } from '@/redux/store';
+import { resetAuth } from '@/redux/features/auth/authSlice';
+import useTimeout from '@/utils/hooks/useTimeout';
+import { timeToMinuteSecond } from '@/utils/helpers';
+import LoadingModal from '@/components/atoms/modals/loading';
 
 const { LOGIN, ONBOARDING, REGISTER } = routesPath;
 
@@ -47,26 +47,33 @@ const SignupPage = () => {
     resendOTP,
     { isLoading: isResendingOTP, isSuccess: OTPResent, reset: resetResendOTP },
   ] = useAdminResendOTPMutation();
-  const [showVerifyOTP, setShowVerifyOTP] = useState(false);
-  const [OTP, setOTP] = useState<any>("");
+  const [OTP, setOTP] = useState<any>('');
 
   const location = usePathname();
   const searchParams = useSearchParams();
-  const ui = searchParams.get("ui");
+  const ui = searchParams.get('ui');
   const router = useRouter();
 
   const disptach = useAppDispatch();
 
+  useEffect(() => {
+    if (!ui) {
+      router.replace(`${REGISTER}`);
+    }
+  }, [ui, location, router]);
+
   const handleFormSubmit = async () => {
-    if (ui === "organization-information") {
+    if (ui === 'organization-information') {
       formik.setErrors({});
       formik.setTouched({});
       router.push(`${location}?ui=employee-information`);
     }
-    if (ui === "employee-information") {
+    if (ui === 'employee-information') {
       register({ ...formik.values })
         .unwrap()
-        .then(() => {});
+        .then(() => {
+          startTimer();
+        });
     }
   };
 
@@ -75,7 +82,7 @@ const SignupPage = () => {
       .unwrap()
       .then(() => {
         toast.success(
-          "Verification code has been successfully sent to your email address"
+          'Verification code has been successfully sent to your email address'
         );
         startTimer();
       });
@@ -85,77 +92,75 @@ const SignupPage = () => {
     adminVerifyOTP({ code: OTP })
       .unwrap()
       .then(() => {
-        // toast.success("OTP Verified Successfully");
-        setShowVerifyOTP(false);
+        resetRegistration();
       });
   };
 
   const formik = useFormik({
     initialValues: {
-      first_name: "",
-      last_name: "",
-      email: "",
-      password: "",
-      designation: "",
-      organization_name: "",
-      employees_range: "",
-      country: "",
-      state: "",
-      address: "",
-      city: "",
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      designation: '',
+      organization_name: '',
+      employees_range: '',
+      country: '',
+      state: '',
+      address: '',
+      city: '',
     },
     validationSchema:
-      RegistrationSchema[ui === "organization-information" ? 0 : 1],
+      RegistrationSchema[ui === 'organization-information' ? 0 : 1],
     onSubmit: handleFormSubmit,
   });
 
   const { timeLeft, startTimer, isTimerElapsed } = useTimeout({
-    initialTime: 30,
+    initialTime: 600,
   });
 
   useEffect(() => {
-    // router.push(`${REGISTER}`)
-    Cookies.remove("token");
+    Cookies.remove('token');
     disptach(resetAuth());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <section className="w-4/6  flex flex-col items-start w-[25rem]">
-      <h1 className=" text-2xl font-semibold mb-4 text-[#162238]">
+    <section className='w-4/6  flex flex-col items-start w-[25rem]'>
+      <h1 className=' text-2xl font-semibold mb-4 text-[#162238]'>
         Register your account
       </h1>
-      <div className="h-[calc(100vh_-_6rem)] w-full pb-10 scroll-hidden overflow-y-auto px-1 ">
+      <div className='h-[calc(100vh_-_6rem)] w-full pb-10 scroll-hidden overflow-y-auto px-1 '>
         <form onSubmit={formik.handleSubmit}>
-          {ui === "organization-information" && (
+          {ui === 'organization-information' && (
             <OrganizationInformation formik={formik} />
           )}
-          {ui === "employee-information" && (
+          {ui === 'employee-information' && (
             <EmployeeInformation formik={formik} />
           )}
 
-          <div className="mt-10">
+          <div className='mt-10'>
             <div>
               <Button
-                className="w-full"
-                type="submit"
+                className='w-full'
+                type='submit'
                 loading={isRegistering}
                 disabled={isRegistering}
                 // loadingText="Create Account"
               >{`${
-                ui === "employee-information" ? "Create Account" : "Next"
+                ui === 'employee-information' ? 'Create Account' : 'Next'
               }`}</Button>
             </div>
             <div>
-              <p className="mt-[18px] mb-6 text-foreground text-[0.8125rem]">
+              <p className='mt-[18px] mb-6 text-foreground text-[0.8125rem]'>
                 By clicking the button above, you agree to our confidential
                 information policies and use of terms. For more information,
-                check out our{" "}
-                <span className="text-primary">Privacy Policy</span>
+                check out our{' '}
+                <span className='text-primary'>Privacy Policy</span>
               </p>
-              <p className="mt-[18px] mb-6 text-foreground text-[0.8125rem]">
-                Already have a Mance account?{" "}
-                <Link href={LOGIN} className="text-primary">
+              <p className='mt-[18px] mb-6 text-foreground text-[0.8125rem]'>
+                Already have a Mance account?{' '}
+                <Link href={LOGIN} className='text-primary'>
                   Log in
                 </Link>
               </p>
@@ -163,90 +168,45 @@ const SignupPage = () => {
           </div>
         </form>
       </div>
-      {/* Email Sent */}
+      {/* Verify Email */}
       <ConfirmationModal
         show={isRegistered}
         handleClose={() => resetRegistration()}
-        hasCloseButton
-        icon="/svgs/mail-sent.svg"
-        title="Check your mailbox !"
+        hasCloseButton={false}
+        title='Verify your email address'
         message={
           <span>
-            We’ve sent an email to{" "}
-            <span className="font-semibold">{formik.values.email}</span> with an
-            OTP to confirm your account. Check your inbox to activate your
-            account.
+            A Six digit OTP code has been sent to your email{' '}
+            <span className='font-semibold'>{formik.values.email}</span>
           </span>
         }
         handleClick={() => {
-          resetRegistration();
-          setShowVerifyOTP(true);
+          handleVerifyOTP(OTP);
         }}
-        actionBtnTitle="Verify Email"
-        footerContent={
-          <>
-            {!isTimerElapsed ? (
-              <div className="text-[#CC0905] text-center text-sm font-normal mt-8">
-                {" "}
-                {timeToMinuteSecond(timeLeft)} mins remaining
-              </div>
-            ) : (
-              <span className="block text-center font-normal mt-8 text-sm text-[#6E7C87]">
-                Didn’t get the code?{" "}
-                <Button
-                  disabled={isResendingOTP}
-                  variant="link"
-                  className="px-0 text-primary font-normal"
-                  onClick={() => handleResendOTP()}
-                >
-                  Resend
-                </Button>{" "}
-              </span>
-            )}
-          </>
-        }
-      />
-      {/* Verify Email */}
-      <ConfirmationModal
-        show={showVerifyOTP}
-        handleClose={() => setShowVerifyOTP(false)}
-        hasCloseButton={false}
-        title="Verify your email address"
-        message={
-          <span>
-            A Six digit OTP code has been sent to your email{" "}
-            <span className="font-semibold">{formik.values.email}</span>
-          </span>
-        }
-        handleClick={() => handleVerifyOTP(OTP)}
-        actionBtnTitle="Verify OTP"
+        actionBtnTitle='Verify OTP'
         actionBtnLoading={isVerifyingOTP}
         disableActionBtn={OTP.length < 6 || isVerifyingOTP}
         content={
-          <div className="my-8 flex justify-center flex-col items-center">
+          <div className='my-8 flex justify-center flex-col items-center'>
             <InputOTPGenerator length={6} onChange={(code) => setOTP(code)} />
+            <div className='text-[#CC0905] text-center text-sm font-normal mt-8'>
+              {timeToMinuteSecond(timeLeft)} mins remaining
+            </div>
           </div>
         }
         footerContent={
           <>
-            {!isTimerElapsed ? (
-              <div className="text-[#CC0905] text-center text-sm font-normal mt-8">
-                {" "}
-                {timeToMinuteSecond(timeLeft)} mins remaining
-              </div>
-            ) : (
-              <span className="block text-center font-normal mt-8 text-sm text-[#6E7C87]">
-                Didn’t get the code?{" "}
-                <Button
-                  disabled={isResendingOTP}
-                  variant="link"
-                  className="px-0 text-primary font-normal"
-                  onClick={() => handleResendOTP()}
-                >
-                  Resend
-                </Button>{" "}
-              </span>
-            )}
+            <span className='block text-center font-normal mt-8 text-sm text-[#6E7C87]'>
+              Didn’t get the code?{' '}
+              <Button
+                disabled={isResendingOTP}
+                variant='link'
+                className='px-0 text-primary font-normal'
+                onClick={() => handleResendOTP()}
+              >
+                Resend
+              </Button>{' '}
+            </span>
           </>
         }
       />
@@ -255,12 +215,12 @@ const SignupPage = () => {
         show={OTPVerified}
         handleClose={() => resetVerifyOTP()}
         hasCloseButton={false}
-        icon="/svgs/mail-verified.svg"
-        title="Welcome, You’re all set !"
+        icon='/svgs/mail-verified.svg'
+        title='Welcome, You’re all set !'
         message={
           <span>
-            Your email address{" "}
-            <span className="font-semibold">{formik.values.email}</span> has
+            Your email address{' '}
+            <span className='font-semibold'>{formik.values.email}</span> has
             been verified. Now, this will be associated with your account. Click
             on the button below to continue
           </span>
@@ -270,15 +230,15 @@ const SignupPage = () => {
           router.push(`${ONBOARDING}?ui=get-started&step=1`);
         }}
         actionBtnTitle={
-          <div className="flex items-center gap-1">
+          <div className='flex items-center gap-1'>
             <span>Setup Organization</span>
             <ArrowRightIcon />
           </div>
         }
         footerContent={
-          <p className="mt-6 font-normal text-[#9AA6AC] text-[11px] text-center">
+          <p className='mt-6 font-normal text-[#9AA6AC] text-[11px] text-center'>
             For more enquiries, contact support
-            <span className="text-primary">@mancesupport.com</span>
+            <span className='text-primary'>@mancesupport.com</span>
           </p>
         }
       />
