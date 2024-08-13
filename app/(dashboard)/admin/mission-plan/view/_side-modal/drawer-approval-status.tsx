0593d4c1a2a8eb76917191cca3d596d64412d9f4
lograshad <akintola.rd@gmail.com>
2024-08-13 12:43:22 +0100

    minor adjustments

--- app/(dashboard)/admin/mission-plan/view/_side-modal/drawer-approval-status.tsx
+++ app/(dashboard)/admin/mission-plan/view/_side-modal/drawer-approval-status.tsx
@@ -1,4 +1,5 @@
 import { ReusableDrawer } from "@/components/fragment";
+import Image from "next/image";
 import React from "react";
 
 export default function DrawerApprovalStatus({
@@ -46,16 +47,19 @@ export default function DrawerApprovalStatus({
               key={index}
               className={`${item.status === "Pending" ? "bg-white border-[var(--primary-color)]" : "bg-[#fafdff] border-[#E5E9EB]"} p-4 border-2 rounded-lg flex justify-between items-center relative`}
             >
-              <div>
+              <div className="flex items-center gap-2">
+                <Image src={item.status === "Pending" ? "/svgs/currApprovalStatus.svg" : "/svgs/approvalStatus.svg"} width={23} height={23} alt="approval status" />
+                <div>
                 <p className="text-sm font-semibold">{item.employeeName}</p>
                 <p className="text-xs font-light text-[#6E7C87]">{item.role}</p>
+                </div>
               </div>
               <div>
                 <p className="text-sm font-semibold">
                   {item.status === "Approved" ? (
                     <span className="text-green-500 py-2 px-3 rounded-lg border border-green-500">Approved</span>
                   ) : item.status === "Pending" ? (
-                    <span className="text-yellow-500 bg-[#eef8f4] py-2 px-3 rounded-lg border border-yellow-500">Pending</span>
+                    <span className="text-yellow-500 bg-[#eef8f4] py-2 px-3 rounded-lg border border-yellow-500">In Review</span>
                   ) :  item.status === "Pending" ? (
                     <span className="text-red-500 bg-[#fffcf6] py-2 px-3 rounded-lg border border-red-500">Rejected</span>
                   ) : (
