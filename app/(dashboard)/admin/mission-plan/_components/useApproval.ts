5a67a872d4184aad87b1dd9347aa1632835d1925
drew-chidi <chidi.andrew@gmail.com>
2024-08-18 14:53:40 +0100

    revert useApproval hook

--- app/(dashboard)/admin/mission-plan/_components/useApproval.ts
+++ app/(dashboard)/admin/mission-plan/_components/useApproval.ts
@@ -1,5 +1,5 @@
 // hooks/useApproval.ts
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useFormik } from "formik";
 import { useApproveMissionPlanItemsMutation } from "@/redux/services/mission-plan/approveItemsApi";
 import { ApprovalItemsSchema } from "@/utils/schema/mission-plan";
@@ -10,12 +10,10 @@ type ApprovablesType = {
   approvable_id: string;
 };
 type Props = {
-  approvableTypeId?: string;
   initialComments: string[];
   initialActionType: string;
   missionplanid: string;
   approval_type?: string;
-  approvables?: ApprovablesType[] | [];
 };
 
 export const useApproval = ({
@@ -23,17 +21,13 @@ export const useApproval = ({
   initialActionType,
   missionplanid,
   approval_type,
-  approvableTypeId,
-  approvables,
 }: Props) => {
-  const [comments, setComments] = useState<string[] | []>();
   const [openCommentId, setOpenCommentId] = useState<string | null>(null);
   const [approveMissionPlanItems] = useApproveMissionPlanItemsMutation();
 
   const FormikApprovalForm = useFormik({
     initialValues: {
-      // comments: [...initialComments],
-      comments: initialComments,
+      comments: [...initialComments],
       newComment: "",
       actionType: initialActionType,
     },
@@ -81,29 +75,11 @@ export const useApproval = ({
     }
   };
 
-  // Get comments
-  // const getCommentsFromApprovables = () => {
-  //   const approvable = approvables?.find(
-  //     (approvable) =>
-  //       approvable?.approvable_id === approvableTypeId &&
-  //       approvable?.comments?.length > 0
-  //   );
-
-  //   if (approvable) {
-  //     setComments([...approvable.comments]);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getCommentsFromApprovables();
-  // }, [approvableTypeId]);
-
   return {
     openCommentId,
     toggleComment,
     handleReject,
     handleApprove,
     FormikApprovalForm,
-    comments,
   };
 };
