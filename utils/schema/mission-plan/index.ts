2017a33a8c1865ed41203c298c2f5fad0a04313a
Odizee <odizeeplatform@gmail.com>
2024-08-19 14:32:05 +0100

    added time range validation to date

--- utils/schema/mission-plan/index.ts
+++ utils/schema/mission-plan/index.ts
@@ -1,4 +1,5 @@
 import { isValidDate } from "@/utils/helpers";
+import { formatToReadableDate } from "@/utils/helpers/date-formatter";
 import * as yup from "yup";
 
 export const missionStatementSchema = yup.object().shape({
@@ -66,56 +67,86 @@ export const setStrategicIntentsSchema = yup.object().shape({
   ),
 });
 
-export const specifiedTaskSchema = yup.object().shape({
-  tasks: yup.array().of(
-    yup.object().shape({
-      id: yup.string().required(),
-      task: yup.string().required("Title is required"),
-      strategic_pillars: yup
-        .array()
-        .of(yup.string().required("Pillar is required"))
-        .min(1, "At least one strategic pillar is required")
-        .required("Strategic pillar is required"),
-      success_measures: yup
-        .array()
-        .of(yup.string().required("Measure is required"))
-        .min(1, "At least one measure is required")
-        .required("Measure is required"),
+export const specifiedTaskSchema = (endDate: any, startDate: any) => {
+  return yup.object().shape({
+    tasks: yup.array().of(
+      yup.object().shape({
+        id: yup.string().required(),
+        task: yup.string().required("Title is required"),
+        strategic_pillars: yup
+          .array()
+          .of(yup.string().required("Pillar is required"))
+          .min(1, "At least one strategic pillar is required")
+          .required("Strategic pillar is required"),
+        success_measures: yup
+          .array()
+          .of(yup.string().required("Measure is required"))
+          .min(1, "At least one measure is required")
+          .required("Measure is required"),
 
-      start_date: yup
-        .string()
-        .required("Start date is required")
-        .typeError("Start date must be a valid date")
-        .test(
-          "is-valid-date",
-          "Start date must be a valid date in DD/MM/YYYY format",
-          (value) => isValidDate(value)
-        ),
-      end_date: yup
-        .string()
-        .required("End date is required")
-        .typeError("End date must be a valid date")
-        .test(
-          "is-valid-date",
-          "End date must be a valid date in DD/MM/YYYY format",
-          (value) => isValidDate(value)
-        ),
-    })
-  ),
-});
+        start_date: yup
+          .string()
+          .required("Start date is required")
+          .typeError("Start date must be a valid date")
+          .test(
+            "is-valid-date",
+            "Start date must be a valid date in DD/MM/YYYY format",
+            (value) => isValidDate(value)
+          )
+          .test(
+            "is-after-min-start-date",
+            `Start date must not be before ${formatToReadableDate(startDate)}`, // Format: DD/MM/YYYY
+            (value) => {
+              if (!value) return true; // Skip this test if value is empty or undefined
+              const dateParts = value.split("/"); // Assuming DD/MM/YYYY format
+              const enteredDate = new Date(
+                `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`
+              );
+              enteredDate.setHours(0, 0, 0, 0);
+              startDate.setHours(0, 0, 0, 0);
+              return enteredDate.getTime() >= startDate.getTime();
+            }
+          ),
+        end_date: yup
+          .string()
+          .required("End date is required")
+          .typeError("End date must be a valid date")
+          .test(
+            "is-valid-date",
+            "End date must be a valid date in DD/MM/YYYY format",
+            (value) => isValidDate(value)
+          )
+          .test(
+            "is-after-end-date",
+            `End date must not be later than ${formatToReadableDate(endDate)}`,
+            (value) => {
+              if (!value) return true;
+              const dateParts = value.split("/");
+              const enteredDate = new Date(
+                `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`
+              );
+              return enteredDate <= endDate;
+            }
+          ),
+      })
+    ),
+  });
+};
 
 export const ApprovalItemsSchema = yup.object().shape({
   comments: yup.array().of(
-    yup.string().test(
-      "comment-required",
-      "Comment is required when rejecting.",
-      function (value) {
-        const { actionType } = this.parent;
-        if (actionType === "reject" && (!value || value.trim() === "")) {
-          return false;
+    yup
+      .string()
+      .test(
+        "comment-required",
+        "Comment is required when rejecting.",
+        function (value) {
+          const { actionType } = this.parent;
+          if (actionType === "reject" && (!value || value.trim() === "")) {
+            return false;
+          }
+          return true;
         }
-        return true;
-      }
-    )
+      )
   ),
 });
