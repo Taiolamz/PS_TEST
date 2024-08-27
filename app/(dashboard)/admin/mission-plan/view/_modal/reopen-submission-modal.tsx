b39d078f7c16aebe60400724bd8a4b1650e78238
Emydev-op <alphaoseghe@gmail.com>
2024-08-27 19:36:48 +0100

    extend ui conversion first stage

--- app/(dashboard)/admin/mission-plan/view/_modal/reopen-submission-modal.tsx
+++ app/(dashboard)/admin/mission-plan/view/_modal/reopen-submission-modal.tsx
@@ -1,16 +1,17 @@
-import CustomDateInput from "@/components/custom-date-input";
-import CustomSelect from "@/components/custom-select";
+import CustomDateInput from "@/components/custom-date-input"; 
+import { CustomMultipleSelect } from "@/components/inputs/custom-multiple-select";
 import ReusableModalContainer from "@/components/reusable-modal-container";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
-import { cn } from "@/lib/utils";
 import { useGetAllEmployeesQuery } from "@/redux/services/employee/employeeApi";
+import { useGetAllOrganizationMissionPlanDropdownQuery } from "@/redux/services/mission-plan/allmissionplanApi";
+import { useAppSelector } from "@/redux/store";
 import { formatDate } from "@/utils/helpers/date-formatter";
 import { useFormik } from "formik";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as Yup from "yup";
 
 interface ModalContainerProps {
@@ -21,7 +22,7 @@ interface ModalContainerProps {
 interface FormValues {
   newEndDate: string;
   staffSelection: string;
-  individualName?: string;
+  multiselectInput: string[] | any[] | string;
   note?: string;
 }
 
@@ -31,7 +32,7 @@ const validationSchema = Yup.object().shape({
     .oneOf(
       [
         "individual",
-        "all-staff",
+        "all_staff",
         "department",
         "unit",
         "branches",
@@ -40,17 +41,14 @@ const validationSchema = Yup.object().shape({
       "Invalid staff selection"
     )
     .required("Please select a staff option"),
-  individualName: Yup.string()
-    .when("staffSelection", {
-      is: (val: string) => val === "individual",
-      then: () => Yup.string().required("Please select a staff name"),
-      otherwise: () => Yup.string().optional(),
-    })
-    .when("staffSelection", {
-      is: (val: string) => val === "department",
-      then: () => Yup.string().required("Please select a staff name"),
-      otherwise: () => Yup.string().optional(),
-    }),
+  multiselectInput: Yup.string().when("staffSelection", {
+    is: (val: string) => val !== "all_staff",
+    then: () =>
+      Yup.array()
+        .min(1, "At least 1 option must be selected")
+        .required("Multiselect input is required"),
+    otherwise: () => Yup.array().optional(),
+  }),
   note: Yup.string().required("Please add a note"),
 });
 
@@ -58,49 +56,78 @@ export default function ReopenSubmissionModal({
   show,
   handleClose,
 }: ModalContainerProps) {
+  const [defaultMultiselect, setDefaultMultiselect] = useState<string[]>([]);
   //Get all staffs
   const { data: employeesData, isLoading: isLoadingEmployees } =
     useGetAllEmployeesQuery();
+  //Get hierarcy dropdown
+  const { data: dropdownData }: any =
+    useGetAllOrganizationMissionPlanDropdownQuery({});
 
-  const handleFormatDropdown = (items: AllStaff[]) => {
+  const handleFormatDropdown = (items: any[] | AllStaff[]) => {
     if (items.length !== 0) {
       const data = items?.map((chi) => {
         return {
           ...chi,
           label: chi?.name,
-          value: chi?.name,
+          value: chi?.id,
         };
       });
-      return [
-        {
-          label: "Staff Name",
-          value: "",
-          name: "",
-          id: "",
-        },
-        ...data,
-      ];
+      return data;
     } else {
       return [];
     }
+  };
+  const handleDropdown = (val: string) => {
+    let option;
+    switch (val) {
+      default:
+        option = handleFormatDropdown([]);
+        break;
+      case "department":
+        option = handleFormatDropdown(
+          dropdownData?.organization_info?.departments
+        );
+        break;
+      case "unit":
+        option = handleFormatDropdown(dropdownData?.organization_info?.units);
+        break;
+      case "individual":
+        option = handleFormatDropdown(employeesData ?? []);
+        break;
+      case "subsidiary":
+        option = handleFormatDropdown(
+          dropdownData?.organization_info?.subsidiaries
+        );
+        break;
+      case "branches":
+        option = handleFormatDropdown(
+          dropdownData?.organization_info?.branches
+        );
+    }
+    return option;
   };
 
   const formik = useFormik<FormValues>({
     initialValues: {
       newEndDate: "",
       staffSelection: "",
-      individualName: "",
+      multiselectInput: [],
       note: "",
     },
     validationSchema,
     onSubmit: (values) => {
-      try { 
+      try {
         // logic for form submission
       } catch (error) {
-        console.error("Form Submission Error:", error); 
+        console.error("Form Submission Error:", error);
       }
     },
   });
+
+  const user_hierarchy = useAppSelector(
+    (state) => state?.auth?.user?.organization?.hierarchy
+  );
 
   useEffect(() => {
     if (show) {
@@ -133,7 +160,7 @@ export default function ReopenSubmissionModal({
               value="March 2022"
               labelClass="text-[#6E7C87] text-[13px] mb-1"
               placeholder="Input new end date"
-              className="border p-2 bg-[#F6F8F9]"
+              className="border p-2 bg-[var(--input-bg)]"
               readOnly
               disabled
             />
@@ -144,7 +171,7 @@ export default function ReopenSubmissionModal({
               value="Feb 2023"
               labelClass="text-[#6E7C87] text-[13px] mb-1"
               placeholder="Input new end date"
-              className="border p-2 bg-[#F6F8F9]"
+              className="border p-2 bg-[var(--input-bg)]"
               readOnly
               disabled
             />
@@ -162,6 +189,7 @@ export default function ReopenSubmissionModal({
                   formik.setFieldValue("newEndDate", formatDate(date));
                 }}
                 placeholder="YYYY/MM/DD"
+                inputClass="bg-[var(--input-bg)]"
                 className="border p-2 bg-[#F6F8F9]"
                 iconClass="top-9"
                 error={formik?.errors?.newEndDate ?? ""}
@@ -183,7 +211,11 @@ export default function ReopenSubmissionModal({
               Select Staff
             </Label>
             <RadioGroup
-              onValueChange={(e) => formik?.setFieldValue("staffSelection", e)}
+              onValueChange={async (e) => {
+               await formik?.setFieldValue("staffSelection", "all_staff");
+                formik?.setFieldValue("multiselectInput", []);
+                formik?.setFieldValue("staffSelection", e);
+              }}
               defaultValue={formik?.values?.staffSelection}
               className="flex space-y-1 mt-3 justify-between items-center w-[90%]"
               id="staffSelection"
@@ -199,50 +231,58 @@ export default function ReopenSubmissionModal({
                 </Label>
               </div>
               <div className="flex items-center space-x-2">
-                <RadioGroupItem value="all-staff" id="all-staff" />
+                <RadioGroupItem value="all_staff" id="all_staff" />
                 <Label
                   className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
-                  htmlFor="all-staff"
+                  htmlFor="all_staff"
                 >
                   All Staff
                 </Label>
               </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="department" id="department" />
-                <Label
-                  className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
-                  htmlFor="department"
-                >
-                  Department
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="unit" id="Unit" />
-                <Label
-                  className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
-                  htmlFor="Unit"
-                >
-                  Unit
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="branches" id="Branches" />
-                <Label
-                  className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
-                  htmlFor="Branches"
-                >
-                  Branches
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="subsidiary" id="subsidiary" />
-                <Label
-                  className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
-                  htmlFor="subsidiary"
-                >
-                  Subsidiary
-                </Label>
-              </div>
+              {user_hierarchy?.includes("department") && (
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="department" id="department" />
+                  <Label
+                    className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
+                    htmlFor="department"
+                  >
+                    Department
+                  </Label>
+                </div>
+              )}
+              {user_hierarchy?.includes("unit") && (
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="unit" id="Unit" />
+                  <Label
+                    className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
+                    htmlFor="Unit"
+                  >
+                    Unit
+                  </Label>
+                </div>
+              )}
+              {user_hierarchy?.includes("branches") && (
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="branches" id="Branches" />
+                  <Label
+                    className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
+                    htmlFor="Branches"
+                  >
+                    Branches
+                  </Label>
+                </div>
+              )}
+              {user_hierarchy?.includes("subsidiary") && (
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="subsidiary" id="subsidiary" />
+                  <Label
+                    className=" text-[13px] text-[#6E7C87] font-normal cursor-pointer"
+                    htmlFor="subsidiary"
+                  >
+                    Subsidiary
+                  </Label>
+                </div>
+              )}
             </RadioGroup>
             {formik?.touched.staffSelection && formik?.errors.staffSelection ? (
               <div className="text-red-500 text-xs">
@@ -251,44 +291,39 @@ export default function ReopenSubmissionModal({
             ) : null}
           </div>
 
-          {formik?.values?.staffSelection === "individual" && (
-            <div className="grid grid-cols-3 gap-x-4">
-              <div>
-                {/* <Input
-                  id="individualName"
-                  name="individualName"
-                  type="text"
-                  value={formik?.values?.individualName}
-                  onChange={formik?.handleChange}
-                  onBlur={formik?.handleBlur}
-                  label="Name of Staff"
-                  labelClass="text-[#6E7C87] text-[13px] mb-[6px]"
-                  placeholder="Input name of staff"
-                  className="border p-2 bg-[#F6F8F9]"
-                  error={formik?.errors?.individualName}
-                /> */}
-                <CustomSelect
-                  label="Name of Staff"
-                  id="individualName"
-                  labelClass="text-[#6E7C87] text-[13px] mb-[6px]"
-                  placeholder="Input name of staff"
-                  options={handleFormatDropdown(employeesData ?? [])}
-                  selected={formik?.values?.individualName ?? ""}
-                  setSelected={(value) => {
-                    formik.setFieldValue("individualName", value);
-                  }}
-                  onBlur={formik?.handleBlur}
-                  // isRequired
-                />
-                {formik?.touched?.individualName &&
-                formik?.errors?.individualName ? (
-                  <div className="text-red-500 text-xs">
-                    {formik?.errors?.individualName}
-                  </div>
-                ) : null}
+          {formik?.values?.staffSelection !== "all_staff" &&
+            formik?.values?.staffSelection !== "" && (
+              <div className="grid grid-cols-3 gap-x-4">
+                <div
+                  className={`${
+                    formik.values.multiselectInput.length > 1
+                      ? "col-span-2"
+                      : ""
+                  }`}
+                >
+                  <CustomMultipleSelect
+                    onValueChange={(values: any) => {
+                      formik.setFieldValue("multiselectInput", values); 
+                      console.log(values, "hfhfhf");
+                    }}
+                    randomBadgeColor
+                    label={`Name of ${formik?.values?.staffSelection}`}
+                    id="multiselectInput"
+                    name="multiselectInput"
+                    labelClass="text-[#6E7C87] text-[13px] block pb-[6px] capitalize"
+                    placeholder={`Select ${formik?.values?.staffSelection}`}
+                    options={handleDropdown(formik?.values?.staffSelection)}
+                    badgeClassName={`rounded-[20px] text-[10px] font-normal`}
+                    triggerClassName={`min-h-[37px] rounded-[6px] border bg-transparent text-sm bg-[#ffffff] border-gray-300 shadow-sm`}
+                    placeholderClass={`font-light text-sm text-[#6E7C87] opacity-70`}
+                    maxCount={6}
+                    onBlur={formik?.handleBlur}
+                    error={formik?.errors?.multiselectInput}
+                    touched={formik?.touched?.multiselectInput} 
+                  />
+                </div>
               </div>
-            </div>
-          )}
+            )}
 
           <div className="grid grid-cols-3 gap-x-4">
             <div className="col-span-2">
@@ -310,9 +345,7 @@ export default function ReopenSubmissionModal({
                 touched={formik?.touched?.note}
                 error={formik?.errors?.note}
               />
-              {/* {formik.touched.note && formik.errors.note ? (
-              <div className="text-red-500 text-xs">{formik.errors.note}</div>
-            ) : null} */}
+
             </div>
           </div>
 
