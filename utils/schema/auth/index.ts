67e1b1048eb23490faa8ed991ff4c01049bfb0c8
drew-chidi <chidi.andrew@gmail.com>
2024-08-27 09:36:14 +0100

    renamed the payload key for confirm password

--- utils/schema/auth/index.ts
+++ utils/schema/auth/index.ts
@@ -65,7 +65,7 @@ export const RegistrationSchema = [
       .test("minUppercase", "Must include small letters", (value) =>
         /[A-Z]/.test(value)
       ),
-    confirm_password: yup
+    password_confirmation: yup
       .string()
       .required("Please confirm your password")
       .oneOf([yup.ref("password"), ""], "Passwords do not match"),
