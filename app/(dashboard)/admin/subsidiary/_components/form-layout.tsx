ce19800dced3d07e887be0b8ee86c6518f202c4e
drew-chidi <chidi.andrew@gmail.com>
2024-08-29 10:07:59 +0100

    cleanup

--- app/(dashboard)/admin/subsidiary/_components/form-layout.tsx
+++ app/(dashboard)/admin/subsidiary/_components/form-layout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
 
 const FormLayout = ({ addText, module, form }: FormLayoutType) => {
   return (
     <div className="mt-10 flex flex-col gap-5 w-[884px] scroll-hidden">
       <p className="text-custom-dark-blue font-normal text-sm">{addText}</p>
-      <p className="text-primary font-normal text-xs mt-5">{`${module} Information`}</p>
+      <p className="text-primary font-normal text-xs mt-5 flex gap-[0.4063rem] items-center">
+        {`${module} Information`} <BsInfoCircleFill fill="#6E7C87" size={12} />
+      </p>
 
       {form}
     </div>
