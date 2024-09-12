5a160a82d341d12b8d6bd229e3dde10899e15c0f
Emydev-op <alphaoseghe@gmail.com>
2024-09-12 11:51:51 +0100

    bug fix for branches

--- app/(dashboard)/admin/branches/_components/form-layout.tsx
+++ app/(dashboard)/admin/branches/_components/form-layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 
 const FormLayout = ({ addText, module, form }: FormLayoutType) => {
   return (
     <div className="mt-10 flex flex-col gap-5 w-[884px] scroll-hidden">
       <p className="text-custom-dark-blue font-normal text-sm">{addText}</p>
-      <p className="text-primary font-normal text-xs mt-5">{`${module} Information`}</p>
+      <p className="text-primary font-normal inline-flex text-xs mt-5">
+        {`${module} Information`}
+        <span className="ml-2 cursor-pointer">
+          <BsFillInfoCircleFill color="#84919A" />
+        </span>
+      </p>
 
       {form}
     </div>
