ba827d2c12fe096dddf98e19db6387b991f61dd7
Odizee <odizeeplatform@gmail.com>
2024-09-24 19:22:21 +0100

    task outcome conversion

---
+++ app/(dashboard)/employee/mission-plan-report/_component/task-outcome-table.tsx
@@ -0,0 +1,100 @@
+import TableWrapper from "@/components/tables/TableWrapper";
+import { usePathname, useRouter } from "next/navigation";
+
+import React, { useCallback } from "react";
+
+interface TaskOutcomeTableProps {
+  FORMAT_TABLE_DATA: (e: any) => void;
+  isFetchingOutcomeData?: boolean;
+  outcomeData: any;
+}
+const TaskOutcomeTable = ({
+  FORMAT_TABLE_DATA,
+  outcomeData,
+}: TaskOutcomeTableProps) => {
+  const pathname = usePathname();
+
+  const router = useRouter();
+
+  const createQueryString = useCallback(
+    (name: string, id: string, type: string) => {
+      const params = new URLSearchParams();
+      params.set("ui", name); // Set the 'name' param
+      params.set("id", id); // Set the 'id' param
+      type === "expected" && params.set("type", "expected-outcome");
+      type === "actual" && params.set("type", "actual-outcome");
+
+      return params.toString(); // Return the query string
+    },
+    [] // We are not dependent on `searchParams` now, so it's an empty dependency array
+  );
+
+  return (
+    <div className="flex flex-col gap-5">
+      <TableWrapper
+        tableheaderList={[
+          "FY Name",
+          "Status",
+          "Approval Progress",
+          "Task Completion Percentage",
+          "Action",
+        ]}
+        perPage={"10"}
+        totalPage={"1"}
+        currentPage={"1"}
+        onPageChange={(p) => {}}
+        onRowClick={() => {}}
+        hideNewBtnOne={true}
+        tableBodyList={FORMAT_TABLE_DATA(outcomeData)}
+        loading={false}
+        dropDown
+        dropDownList={[
+          {
+            label: "January Expected Outcome",
+            color: "",
+            onActionClick: (param: any, dataTwo: any) => {
+              //   console.log(
+              //     "dataTwo",
+              //     dataTwo?.name?.props?.children[0].props.children
+              //   );
+
+              router.push(
+                pathname.split("?")[0] +
+                  "?" +
+                  createQueryString(
+                    "task_outcome",
+                    dataTwo?.name?.props?.children[0].props.children,
+                    "expected"
+                  )
+              );
+            },
+          },
+          {
+            label: "January Actual Outcome",
+            color: "",
+            onActionClick: (param: any, dataTwo: any) => {
+              router.push(
+                pathname.split("?")[0] +
+                  "?" +
+                  createQueryString(
+                    "task_outcome",
+                    dataTwo?.name?.props?.children[0].props.children,
+                    "actual"
+                  )
+              );
+            },
+          },
+          {
+            label: "Approval Status",
+            color: "",
+            onActionClick: (param: any, dataTwo: any) => {},
+          },
+        ]}
+        hideSearchFilterBox
+        width="240px"
+      />
+    </div>
+  );
+};
+
+export default TaskOutcomeTable;
--- app/(dashboard)/employee/mission-plan-report/_partials/_task_outcome/task_outcome.tsx
+++ app/(dashboard)/employee/mission-plan-report/_partials/_task_outcome/task_outcome.tsx
@@ -1,7 +1,99 @@
+import { Progress } from "@/components/ui/progress";
+import TaskOutcomeTable from "../../_component/task-outcome-table";
+import ApprovalProgress from "@/components/custom-progress/progress-bars";
+import { useSearchParams } from "next/navigation";
+
+const data = [
+  {
+    id: 1,
+    fyName: "FY 2024",
+    status: "Ongoing",
+    approvalProgress: 1,
+    taskCompletionPercentage: 60,
+  },
+  {
+    id: 1,
+    fyName: "Financial Year 2023",
+    status: "Closed",
+    approvalProgress: 4,
+    taskCompletionPercentage: 100,
+  },
+  {
+    id: 1,
+    fyName: "FY 2022",
+    status: "Ongoing",
+    approvalProgress: 5,
+    taskCompletionPercentage: 70,
+  },
+  {
+    id: 1,
+    fyName: "FY 2021",
+    status: "Ongoing",
+    approvalProgress: 3,
+    taskCompletionPercentage: 39,
+  },
+];
+
 const TaskOutcome = () => {
+  const searchParams = useSearchParams();
+  const ui = searchParams.get("ui");
+  const id = searchParams.get("id");
+  const type = searchParams.get("type");
+
+  const FORMAT_TABLE_DATA = (data: any) => {
+    return data?.map((item: any) => ({
+      name: (
+        <>
+          <span className="hidden">{item.id}</span>
+          <p>{item?.fyName}</p>
+        </>
+      ),
+      status: (
+        <p
+          className={
+            item?.status === "Ongoing" ? "text-[#119C2B]" : "text-[#3E4345]"
+          }
+        >
+          {item?.status}
+        </p>
+      ),
+      approvalProgress: (
+        <ApprovalProgress steps={5} completedSteps={item?.approvalProgress} />
+      ),
+
+      taskCompletionPercentage: (
+        <div className="flex gap-x-1 items-center">
+          <p className="text-[10px]">{item?.taskCompletionPercentage}%</p>
+          <Progress
+            value={item?.taskCompletionPercentage}
+            className={`w-[150px] h-2 `}
+            indicatorClass={
+              item?.taskCompletionPercentage >= 70
+                ? "bg-green-500"
+                : item?.taskCompletionPercentage > 40
+                ? "bg-warning"
+                : "bg-[red]"
+            }
+          />
+        </div>
+      ),
+    }));
+  };
+
   return (
-    <div>
-      <p>TaskOutcome</p>
+    <div className="mt-11">
+      {ui === "task_outcome" && !id && (
+        <TaskOutcomeTable
+          FORMAT_TABLE_DATA={FORMAT_TABLE_DATA}
+          outcomeData={data}
+        />
+      )}
+      {ui === "task_outcome" && id && type === "expected-outcome" && (
+        <div> Expected Outcome </div>
+      )}
+      {ui === "task_outcome" && id && type === "actual-outcome" && (
+        <div> Actual Outcome </div>
+      )}
     </div>
   );
 };
