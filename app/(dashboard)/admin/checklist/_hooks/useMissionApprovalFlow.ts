959ed48a3941c938f2f9a9ab56244c6ae02a6ff5
taiolamz <hassandefarati@gmail.com>
2024-10-13 01:50:12 +0100

    approval flow update

--- app/(dashboard)/admin/checklist/_hooks/useMissionApprovalFlow.ts
+++ app/(dashboard)/admin/checklist/_hooks/useMissionApprovalFlow.ts
@@ -12,6 +12,7 @@ import { useGetGradeLevelsQuery } from "@/redux/services/onboarding/gradeLevelAp
 
 type Prop = {
   cancelPath: string;
+  id?: string;
 };
 
 type Select = {
@@ -46,7 +47,7 @@ const formSchema = yup.object().shape({
     .required("Order of approvals is required"),
 });
 const { ADMIN } = routesPath;
-export const useMissionApprovalFlow = ({ cancelPath }: Prop) => {
+export const useMissionApprovalFlow = ({ cancelPath, id }: Prop) => {
   const handleFormatDropdown = (items: Select[]) => {
     const data = items.map((chi) => {
       return {
@@ -91,7 +92,11 @@ export const useMissionApprovalFlow = ({ cancelPath }: Prop) => {
     await createMissionFlow(payload)
       .unwrap()
       .then(() => {
-        toast.success("Approval Flow Created Successfully");
+        toast.success(
+          id
+            ? "Approval Flow Updated Successfully"
+            : "Approval Flow Created Successfully"
+        );
         new Promise(() => {
           setTimeout(() => {
             toast.dismiss();
