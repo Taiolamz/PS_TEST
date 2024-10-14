8578c38f7af67f05f11afed94cdb36af15fe6945
taiolamz <hassandefarati@gmail.com>
2024-10-14 18:17:07 +0100

    switching branch

--- app/(dashboard)/employee/mission-plan-report/_partials/_downlines/_partials/downline-table.tsx
+++ app/(dashboard)/employee/mission-plan-report/_partials/_downlines/_partials/downline-table.tsx
@@ -1,105 +1,73 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import routesPath from "@/utils/routes";
 import { useRouter } from "next/navigation";
 import TableWrapper from "@/components/tables/TableWrapper";
 import { formatDate } from "@/utils/helpers/date-formatter";
 import BadgeComponent from "@/components/badge/BadgeComponents";
-import { useAppSelector } from "@/redux/store";
-import { TableLoader } from "@/components/fragment";
-import { PageLoader } from "@/components/custom-loader";
-import { useGetOrganizationMissionPlansQuery } from "@/redux/services/mission-plan/missionPlanApi";
-import { useGetDownlinerMissionPlanReportQuery } from "@/redux/services/mission-plan/reports/employee/missionPlanReportApi";
 
 const { EMPLOYEE } = routesPath;
 
 export default function DownlineTable() {
-  const [search, setSearch] = useState<string>("");
-  // const [filter, setFilter] = useState<string>("");
-  // const [sort, setSort] = useState<string>("");
   const router = useRouter();
-
-  const { data, isLoading, isFetching } =
-    useGetDownlinerMissionPlanReportQuery<{
-      data: { data: any[]; links: any; meta: any };
-      isLoading: boolean;
-      isFetching?: boolean;
-    }>({
-      search: search,
-    });
+  
 
   return (
-    <>
-      {isLoading ? (
-        <div className="h-[75vh] grid place-content-center">
-          <PageLoader />
-        </div>
-      ) : (
-        <TableWrapper
-          tableheaderList={[
-            "Staff Name",
-            "Job Title",
-            "Email",
-            "Date Submitted",
-            "Approval Status",
-            "Action",
-          ]}
-          TableTitle="Downline Progress"
-          perPage={"10"}
-          totalPage={"1"}
-          currentPage={"1"}
-          // onPageChange={(p) => {}}
-          onRowClick={() => {}}
-          hideNewBtnOne={true}
-          tableBodyList={FORMAT_TABLE_DATA(data?.data)}
-          dropDown
-          loading={isFetching}
-          onPageChange={(p) => {
-            console.log(p);
-          }}
-          onSearch={(val) => {
-            setTimeout(() => {
-              setSearch(val);
-            }, 2000);
-          }}
-          dropDownList={[
-            {
-              label: "View Progress",
-              color: "",
-              onActionClick: (param: any, dataTwo: any) => {
-                router.push(
-                  EMPLOYEE.DOWNLINE_MISSION_PLAN_REPORT(
-                    dataTwo?.staff_name?.props?.children[0].props.children
-                  )
-                );
-              },
-            },
-            {
-              label: "View Measure of Success Submission",
-              onActionClick: (param: any, dataTwo: any) => {
-                router.push(
-                  EMPLOYEE.REVIEW_MOS(
-                    dataTwo?.staff_name?.props?.children[0].props.children
-                  )
-                );
-              },
-            },
-            {
-              label: "View Task Submission",
-              onActionClick: (param: any, dataTwo: any) => {
-                router.push(
-                  EMPLOYEE.REVIEW_TASK(
-                    dataTwo?.staff_name?.props?.children[0].props.children
-                  )
-                );
-              },
-            },
-          ]}
-          width="300px"
-          hideSearchFilterBox
-        />
-      )}
-    </>
+    <TableWrapper
+      tableheaderList={[
+        "Staff Name",
+        "Job Title",
+        "Email",
+        "Date Submitted",
+        "Approval Status",
+        "Action",
+      ]}
+      TableTitle="Downline Progress"
+      perPage={"10"}
+      totalPage={"1"}
+      currentPage={"1"}
+      onPageChange={(p) => {}}
+      onRowClick={() => {}}
+      hideNewBtnOne={true}
+      tableBodyList={FORMAT_TABLE_DATA(data)}
+      loading={false}
+      dropDown
+      dropDownList={[
+        {
+          label: "View Progress",
+          color: "",
+          onActionClick: (param: any, dataTwo: any) => {
+            router.push(
+              EMPLOYEE.DOWNLINE_MISSION_PLAN_REPORT(
+                dataTwo?.staff_name?.props?.children[0].props.children
+              )
+            );
+          },
+        },
+        {
+          label: "View Measure of Success Submission",
+          onActionClick: (param: any, dataTwo: any) => {
+            router.push(
+              EMPLOYEE.REVIEW_MOS(
+                dataTwo?.staff_name?.props?.children[0].props.children
+              )
+            );
+          },
+        },
+        {
+          label: "View Task Submission",
+          onActionClick: (param: any, dataTwo: any) => {
+            router.push(
+              EMPLOYEE.REVIEW_TASK(
+                dataTwo?.staff_name?.props?.children[0].props.children
+              )
+            );
+          },
+        },
+      ]}
+      width="300px"
+      hideSearchFilterBox
+    />
   );
 }
 
@@ -143,19 +111,19 @@ const FORMAT_TABLE_DATA = (data: any) => {
     staff_name: (
       <>
         <span className="hidden">{item.id}</span>
-        <p>{item?.name}</p>
+        <p>{item?.staff_name}</p>
       </>
     ),
-    job_title: item?.desgination || "n/a",
-    work_email: item?.email || "n/a",
-    created_at: formatDate(item?.date_submitted),
+    job_title: item.job_title,
+    work_email: item.work_email,
+    created_at: formatDate(item?.created_at),
     status: (
       <BadgeComponent
         text={item?.status}
         color={
-          item?.status?.toLowerCase() === "success"
+          item?.status?.toLowerCase() === "approved"
             ? "green"
-            : item?.status?.toLowerCase() === "failed"
+            : item?.status?.toLowerCase() === "rejected"
             ? "red"
             : "yellow"
         }
