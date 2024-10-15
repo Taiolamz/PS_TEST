f64a110c87b9ce0366e11e1e51f4f5d2207c21d0
taiolamz <hassandefarati@gmail.com>
2024-10-15 20:26:15 +0100

    branch detail view completion

--- app/(dashboard)/admin/branches/_partials/_table/staff.tsx
+++ app/(dashboard)/admin/branches/_partials/_table/staff.tsx
@@ -1,22 +1,44 @@
 "use client";
+import { TableLoader } from "@/components/fragment";
 import TableWrapper from "@/components/tables/TableWrapper";
+import { useGetSubsidiaryInStaffQuery } from "@/redux/services/checklist/subsidiaryApi";
 import routesPath from "@/utils/routes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const { ADMIN } = routesPath;
 
-interface StaffTableProps {
-  data?: any[];
-}
-
-export default function StaffTable({ data }: StaffTableProps) {
+export default function StaffTable({
+  isLoading,
+  subDetailsData,
+  tableData,
+  isFetching,
+  onSearch,
+  perPage,
+  totalPage,
+  currentPage,
+  onPageChange,
+}: {
+  perPage?: number;
+  totalPage?: number;
+  currentPage?: number;
+  isLoading?: boolean;
+  subDetailsData?: any;
+  tableData?: any[];
+  isFetching?: boolean;
+  onSearch?: (param: string) => void;
+  onPageChange?: (param: string) => void;
+}) {
   const router = useRouter();
+
   const handleAddStaff = () => {
     const path = ADMIN.ADD_EMPLOYEE;
     router.push(path);
   };
-  return (
+
+  return isLoading ? (
+    <TableLoader rows={8} columns={8} />
+  ) : (
     <TableWrapper
       tableheaderList={[
         "Staff Name",
@@ -27,31 +49,45 @@ export default function StaffTable({ data }: StaffTableProps) {
         "Line Manager",
         "Action",
       ]}
-      hidePagination
+      perPage={perPage}
+      totalPage={totalPage}
+      currentPage={currentPage}
+      onPageChange={onPageChange}
       addText="New Staff"
-      hideNewBtnOne={false}
-      tableBodyList={data}
-      loading={false}
-      onSearch={(param) => {
-        console.log(param);
-      }}
+      newBtnBulk={!subDetailsData?.deleted_at}
+      hideNewBtnOne={subDetailsData?.deleted_at}
+      tableBodyList={FORMAT_TABLE_DATA(tableData)}
+      loading={isFetching}
+      onSearch={onSearch}
       dropDown
       hideFilter
       hideSort
-      newBtnBulk
+      // dropDownList={[
+      //   {
+      //     label: "View Details",
+      //     color: "",
+      //     onActionClick: (param: any, dataTwo: any) => {
+      //       // router.push(
+      //       //   pathname +
+      //       //     "?" +
+      //       //     "ui=details" +
+      //       //     "&" +
+      //       //     "id=" +
+      //       //     dataTwo?.name?.props.children[0].props.children
+      //       // );
+      //     },
+      //   },
+      // ]}
       dropDownList={[
         {
-          label: "View Details",
+          label: <span className="text-xs"> View Details </span>,
           color: "",
-          onActionClick: (param: any, dataTwo: any) => {
-            // router.push(
-            //   pathname +
-            //     "?" +
-            //     "ui=details" +
-            //     "&" +
-            //     "id=" +
-            //     dataTwo?.name?.props.children[0].props.children
-            // );
+          onActionClick: (param: any, data: any) => {
+            router.push(
+              routesPath?.ADMIN?.EMPLOYEE_VIEW(
+                data?.name?.props?.children[0]?.props?.children
+              )
+            );
           },
         },
       ]}
@@ -62,3 +98,19 @@ export default function StaffTable({ data }: StaffTableProps) {
     />
   );
 }
+
+const FORMAT_TABLE_DATA = (obj: any) => {
+  return obj?.map((org: any) => ({
+    name: (
+      <>
+        <span className="hidden">{org?.id}</span>
+        <p>{org?.name}</p>
+      </>
+    ),
+    gender: org?.gender || "n/a",
+    email: org?.email || "n/a",
+    job_title: org?.job_title || "n/a",
+    role: org?.role || "n/a",
+    line_manager_name: org?.line_manager_name || "n/a",
+  }));
+};
