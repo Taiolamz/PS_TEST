df97c1edf06d7741654d977a53106c313ed4ebd8
Odoh Friday <odohfriday33@yahoo.com>
2024-10-16 12:06:36 +0100

    integrated extend target

--- redux/services/mission-plan/reports/admin/targetOutcomeApi.ts
+++ redux/services/mission-plan/reports/admin/targetOutcomeApi.ts
@@ -6,9 +6,17 @@ export const targetOutcomeApi = baseApi.injectEndpoints({
       query: (payload) => ({
         url: `/admin/mission-plan-report/settings/target`,
         method: "POST",
-        body: payload
+        body: payload,
       }),
-      invalidatesTags: ['TargetOutcomeSettings']
+      invalidatesTags: ["TargetOutcomeSettings"],
+    }),
+    extendTargetSubmission: builder.mutation({
+      query: (payload) => ({
+        url: `/admin/mission-plan-report/settings/re-open-target`,
+        method: "POST",
+        body: payload,
+      }),
+      invalidatesTags: ["TargetOutcomeSettings"],
     }),
     setActualOutcomeSubmission: builder.mutation({
       query: (payload) => ({
@@ -16,20 +24,21 @@ export const targetOutcomeApi = baseApi.injectEndpoints({
         method: "POST",
         body: payload,
       }),
-      invalidatesTags: ['TargetOutcomeSettings']
+      invalidatesTags: ["TargetOutcomeSettings"],
     }),
     fetchReportSubmissionSettings: builder.query({
       query: () => ({
         url: `/admin/mission-plan-report/settings`,
         method: "GET",
       }),
-      providesTags: ['TargetOutcomeSettings']
+      providesTags: ["TargetOutcomeSettings"],
     }),
   }),
 });
 
 export const {
-    useSetExpectedOutcomeSubmissionMutation,
-    useSetActualOutcomeSubmissionMutation,
-    useFetchReportSubmissionSettingsQuery
+  useSetExpectedOutcomeSubmissionMutation,
+  useSetActualOutcomeSubmissionMutation,
+  useFetchReportSubmissionSettingsQuery,
+  useExtendTargetSubmissionMutation,
 } = targetOutcomeApi;
